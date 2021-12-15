f3c54370629f66858932ce745d1acc6629b3fd35
JuanDa237 <juandavid200301@hotmail.com>
2021-12-15 01:41:58 -0500

    - I separated landing in components

--- src/app/modules/landing/components/index.ts
+++ src/app/modules/landing/components/index.ts
@@ -1,7 +1,16 @@
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { WordCounterComponent } from './word-counter/word-counter.component';
 
-export const components = [NavBarComponent, FooterComponent];
+export const components = [
+	NavBarComponent,
+	FooterComponent,
+	IntroductionComponent,
+	WordCounterComponent
+];
 
 export * from './nav-bar/nav-bar.component';
 export * from './footer/footer.component';
+export * from './introduction/introduction.component';
+export * from './word-counter/word-counter.component';
---
+++ src/app/modules/landing/components/word-counter/word-counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-word-counter',
+  templateUrl: './word-counter.component.html',
+  styleUrls: ['./word-counter.component.scss']
+})
+export class WordCounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
